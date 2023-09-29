853264dc45eb6921a7365bf02d7bb679afa84ee1
codearcher2112 <haykak5@gmail.com>
2023-09-29 18:40:20 +0400

    Create ( formatDate Function ) Create function which format date

---
+++ dateFormatDate.js
@@ -0,0 +1,11 @@
+const formatDate = (date) => {
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+
+    return `${day} ${monthNames[monthIndex]} ${year}`;
+}
+
+formatDate(new Date("2020-05-14"));
