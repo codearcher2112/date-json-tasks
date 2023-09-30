467e57211b238f5605cd3f243b536e7532fe2082
codearcher2112 <haykak5@gmail.com>
2023-09-30 08:31:17 +0400

    Initial commit for monthsInterval function

---
+++ dateMonthsInterval.js
@@ -0,0 +1,10 @@
+let january = new Date(2017, 0, 1);
+let march = new Date(2018, 2, 1); 
+
+function monthsInterval(startDate, endDate) {
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    
+}
+
+monthsInterval(january, march);
